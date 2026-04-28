fbda8cc0ddc223b17347f7eabf67d40a91b0b697
Kyosuke Sawazaki <sawazaki@the-triad.jp>
2026-04-28 09:32:24 +0900

    ファイルの更新

--- members_export.php
+++ members_export.php
@@ -47,7 +47,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && ($_POST['action'] ?? '') === 'expor
         $row = [];
         foreach ($selected as $key) {
             if ($key === 'grade') {
-                $row[] = $m['grade'] . '年';
+                $row[] = $m['grade'];
             } else {
                 $row[] = csv_safe($m[$key] ?? '');
             }
